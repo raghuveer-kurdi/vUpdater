81607fd32cabb4dc4983d668ade0857d8d85ee47
Raghuveer K <raghuveer.k@BRILLIO.COM>
2019-03-06 15:00:13 +0530

    some more code

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ import (
 func main() {
 	var files []string
 
-	root := "c:\\Users\\raghu\\Desktop\\Temp"
+	root := "c:\\Users\\raghuveer.k\\Desktop\\Temp"
 	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
 		if info.IsDir() || (filepath.Ext(path) != ".html" && filepath.Ext(path) != ".htm") {
 			return nil
@@ -24,7 +24,10 @@ func main() {
 		panic(err)
 	}
 
-	re := regexp.MustCompile(`.js(\?*.*\d+)`)
+	jsDQuote := regexp.MustCompile(`\.js(\?*.*\d*")`)
+	jsSQuote := regexp.MustCompile(`\.js(\?*.*\d*')`)
+	cssDQuote := regexp.MustCompile(`\.css(\?*\w*=*\d*")`)
+	cssSQuote := regexp.MustCompile(`\.css(\?*\w*=*\d*')`)
 	for _, file := range files {
 		fileContents, err := ioutil.ReadFile(file)
 		if err != nil {
@@ -32,9 +35,16 @@ func main() {
 			os.Exit(2)
 		}
 		lines := strings.Split(string(fileContents), "\n")
+		fmt.Println("--------file name : ", file, "-----------------")
 		for _, line := range lines {
-			if strings.Contains(line, "script") {
-				fmt.Println(re.ReplaceAllString(line, ".js?v=123"))
+			if jsDQuote.MatchString(line) {
+				fmt.Println(jsDQuote.ReplaceAllString(line, ".js?v=123\""))
+			} else if jsSQuote.MatchString(line) {
+				fmt.Println(jsSQuote.ReplaceAllString(line, ".js?v=123'"))
+			} else if cssDQuote.MatchString(line) {
+				fmt.Println(cssDQuote.ReplaceAllString(line, ".css?v=123\""))
+			} else if cssSQuote.MatchString(line) {
+				fmt.Println(cssDQuote.ReplaceAllString(line, ".css?v=123'"))
 			}
 		}
 	}
